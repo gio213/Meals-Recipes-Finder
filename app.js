4dd19b7daa04dc367ddce0946edc446701807032
Giorgi Patsia <62309418+gio213@users.noreply.github.com>
2023-03-27 09:24:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,28 +10,37 @@ const secondContainer = document.getElementById("secondContainer");
 secondContainer.style.display = "none";
 const searchInput = document.querySelector("input");
 const button = document.getElementById("button");
+const categoryElement = document.getElementById("category");
+
+let temp = [];
+
+const rendrerData = async (data) => {
+  const box = document.createElement("div");
+  const img = document.createElement("img");
+  const title = document.createElement("h3");
+
+  box.setAttribute("class", "box");
+  const meal = data;
+  title.innerHTML = meal.strMeal;
+
+  img.style.border = "2px solid black";
+  img.src = meal.strMealThumb;
+  img.alt = meal.strMeal;
+  img.data = meal;
+
+  box.appendChild(img);
+
+  box.appendChild(title);
+  container.appendChild(box);
+};
 
 async function getMeal() {
   for (let i = 0; i < 8; i++) {
-    const box = document.createElement("div");
-    box.setAttribute("class", "box");
-
-    const title = document.createElement("h3");
-
     const response = await fetch(url);
     const data = await response.json();
-    const meal = data.meals[0];
-    title.innerHTML = meal.strMeal;
-    img = document.createElement("img");
-    img.style.border = "2px solid black";
-    img.src = meal.strMealThumb;
-    img.alt = meal.strMeal;
-    img.data = meal;
+    temp.push(data);
 
-    box.appendChild(img);
-
-    box.appendChild(title);
-    container.appendChild(box);
+    rendrerData(data.meals[0]);
   }
   container.childNodes.forEach((box) => {
     box.addEventListener("click", (e) => {
@@ -40,8 +49,6 @@ async function getMeal() {
       secondContainer.setAttribute("class", "secondContainer");
 
       const data = e.target.data;
-      
-      
 
       const mealImg = e.target.src;
 
@@ -127,6 +134,7 @@ getMeal();
 button.addEventListener("click", () => {
   container.innerHTML = "";
   searchInput.value = "";
+  categoryElement[0].selected = true;
   getMeal();
   if (secondContainer.style.display === "flex") {
     secondContainer.style.display = "flex";
@@ -140,130 +148,137 @@ button.addEventListener("click", () => {
 
 searchInput.addEventListener("keyup", (e) => {
   const searchValue = e.target.value;
-  
+
   async function getSearchedMeal() {
     const response = await fetch(
       `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
     );
     const data = await response.json();
     const meal = data.meals[0];
-    
 
-    container.innerHTML = "";
-    const box = document.createElement("div");
-    box.setAttribute("class", "box");
+    if (searchValue.length > 0) {
+      container.innerHTML = "";
 
-    const title = document.createElement("h3");
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
 
-    title.innerHTML = meal.strMeal;
-    img = document.createElement("img");
-    img.style.border = "2px solid black";
-    img.src = meal.strMealThumb;
-    img.alt = meal.strMeal;
-    img.data = meal;
-    
+      const title = document.createElement("h3");
 
-    box.appendChild(img);
+      title.innerHTML = meal.strMeal;
+      img = document.createElement("img");
+      img.style.border = "2px solid black";
+      img.src = meal.strMealThumb;
+      img.alt = meal.strMeal;
+      img.data = meal;
 
-    box.appendChild(title);
-    container.appendChild(box);
-    container.childNodes.forEach((box) => {
-      box.addEventListener("click", (e) => {
-        container.style.display = "none";
-        secondContainer.style.display = "flex";
-        secondContainer.setAttribute("class", "secondContainer");
+      box.appendChild(img);
 
-        const data = e.target.data;
-        
+      box.appendChild(title);
+      container.appendChild(box);
+      container.childNodes.forEach((box) => {
+        box.addEventListener("click", (e) => {
+          container.style.display = "none";
+          secondContainer.style.display = "flex";
+          secondContainer.setAttribute("class", "secondContainer");
 
-        const mealImg = e.target.src;
+          const data = e.target.data;
 
-        const mealImgDiv = document.createElement("div");
-        mealImgDiv.setAttribute("class", "mealImgDiv");
-        const mealImgTag = document.createElement("img");
-        mealImgDiv.appendChild(mealImgTag);
-        mealImgTag.src = mealImg;
-        secondContainer.appendChild(mealImgDiv);
+          const mealImg = e.target.src;
 
-        const mealTitleDiv = document.createElement("div");
-        mealTitleDiv.setAttribute("class", "mealTitleDiv");
-        const mealTitle = document.createElement("h1");
-        mealTitle.innerHTML = data.strMeal;
-        mealTitleDiv.appendChild(mealTitle);
-        secondContainer.appendChild(mealTitleDiv);
-        const insturctionDiv = document.createElement("div");
-        insturctionDiv.setAttribute("class", "insturctionDiv");
-        const insturction = document.createElement("p");
-        insturction.innerHTML = data.strInstructions;
-        insturctionDiv.appendChild(insturction);
-        secondContainer.appendChild(insturctionDiv);
-        const titleAndInsturctionDiv = document.createElement("div");
-        titleAndInsturctionDiv.setAttribute("class", "titleAndInsturctionDiv");
-        titleAndInsturctionDiv.appendChild(mealTitleDiv);
-        titleAndInsturctionDiv.appendChild(insturctionDiv);
-        secondContainer.appendChild(titleAndInsturctionDiv);
-        const infoDiv = document.createElement("div");
-        infoDiv.setAttribute("class", "infoDiv");
-        const info = document.createElement("ul");
-        info.innerHTML = `
-        <li>Area: ${data.strArea}</li>
-        <li>Category: ${data.strCategory}</li>
-        <li>Tags: ${data.strTags}</li>
-        `;
-        if (data.strTags === null) {
-          info.innerHTML = ` 
-            <li>Area: ${data.strArea}</li>
-            <li>Category: ${data.strCategory}</li>
-            <li>Tags: No tags</li>
+          const mealImgDiv = document.createElement("div");
+          mealImgDiv.setAttribute("class", "mealImgDiv");
+          const mealImgTag = document.createElement("img");
+          mealImgDiv.appendChild(mealImgTag);
+          mealImgTag.src = mealImg;
+          secondContainer.appendChild(mealImgDiv);
+
+          const mealTitleDiv = document.createElement("div");
+          mealTitleDiv.setAttribute("class", "mealTitleDiv");
+          const mealTitle = document.createElement("h1");
+          mealTitle.innerHTML = data.strMeal;
+          mealTitleDiv.appendChild(mealTitle);
+          secondContainer.appendChild(mealTitleDiv);
+          const insturctionDiv = document.createElement("div");
+          insturctionDiv.setAttribute("class", "insturctionDiv");
+          const insturction = document.createElement("p");
+          insturction.innerHTML = data.strInstructions;
+          insturctionDiv.appendChild(insturction);
+          secondContainer.appendChild(insturctionDiv);
+          const titleAndInsturctionDiv = document.createElement("div");
+          titleAndInsturctionDiv.setAttribute(
+            "class",
+            "titleAndInsturctionDiv"
+          );
+          titleAndInsturctionDiv.appendChild(mealTitleDiv);
+          titleAndInsturctionDiv.appendChild(insturctionDiv);
+          secondContainer.appendChild(titleAndInsturctionDiv);
+          const infoDiv = document.createElement("div");
+          infoDiv.setAttribute("class", "infoDiv");
+          const info = document.createElement("ul");
+          info.innerHTML = `
+          <li>Area: ${data.strArea}</li>
+          <li>Category: ${data.strCategory}</li>
+          <li>Tags: ${data.strTags}</li>
           `;
-        }
-
-        infoDiv.appendChild(info);
-        mealImgDiv.appendChild(infoDiv);
-        const ingredientsTitle = document.createElement("h2");
-        ingredientsTitle.innerHTML = "Ingredients:";
-        ingredientsTitle.setAttribute("class", "ingredientsTitle");
-        mealImgDiv.appendChild(ingredientsTitle);
-
-        const ingredientsDiv = document.createElement("div");
-        ingredientsDiv.setAttribute("class", "ingredientsDiv");
-        const ingredients = document.createElement("ul");
-        ingredientsDiv.appendChild(ingredients);
-        mealImgDiv.appendChild(ingredientsDiv);
-
-        for (let i = 1; i < 20; i++) {
-          if (data[`strIngredient${i}`] !== "") {
-            const ingredient = document.createElement("li");
-            ingredient.innerHTML = `${data[`strIngredient${i}`]} - ${
-              data[`strMeasure${i}`]
-            }`;
-            ingredients.appendChild(ingredient);
+          if (data.strTags === null) {
+            info.innerHTML = `
+              <li>Area: ${data.strArea}</li>
+              <li>Category: ${data.strCategory}</li>
+              <li>Tags: No tags</li>
+            `;
           }
-        }
 
-        const youtubeIfame = document.createElement("iframe");
-        youtubeIfame.setAttribute("class", "youtubeIfame");
-        youtubeIfame.src = `https://www.youtube.com/embed/${data.strYoutube.slice(
-          -11
-        )}`;
-        if (data.strYoutube === null) {
-          youtubeIfame.style.display = "none";
-        }
+          infoDiv.appendChild(info);
+          mealImgDiv.appendChild(infoDiv);
+          const ingredientsTitle = document.createElement("h2");
+          ingredientsTitle.innerHTML = "Ingredients:";
+          ingredientsTitle.setAttribute("class", "ingredientsTitle");
+          mealImgDiv.appendChild(ingredientsTitle);
 
-        titleAndInsturctionDiv.appendChild(youtubeIfame);
+          const ingredientsDiv = document.createElement("div");
+          ingredientsDiv.setAttribute("class", "ingredientsDiv");
+          const ingredients = document.createElement("ul");
+          ingredientsDiv.appendChild(ingredients);
+          mealImgDiv.appendChild(ingredientsDiv);
+
+          for (let i = 1; i < 20; i++) {
+            if (data[`strIngredient${i}`] !== "") {
+              const ingredient = document.createElement("li");
+              ingredient.innerHTML = `${data[`strIngredient${i}`]} - ${
+                data[`strMeasure${i}`]
+              }`;
+              ingredients.appendChild(ingredient);
+            }
+          }
+
+          const youtubeIfame = document.createElement("iframe");
+          youtubeIfame.setAttribute("class", "youtubeIfame");
+          youtubeIfame.src = `https://www.youtube.com/embed/${data.strYoutube.slice(
+            -11
+          )}`;
+          if (data.strYoutube === null) {
+            youtubeIfame.style.display = "none";
+          }
+
+          titleAndInsturctionDiv.appendChild(youtubeIfame);
+        });
       });
-    });
+    } else {
+      container.innerHTML = "";
+      getMeal();
+    }
   }
   getSearchedMeal();
 });
+
+let selectedCategory;
+const select = document.getElementById("category");
 
 async function getMealByCategory() {
   fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     .then((response) => response.json())
     .then((data) => {
       const categories = data.categories;
-     
-      const select = document.getElementById("category");
       categories.forEach((category) => {
         const option = document.createElement("option");
         option.value = category.strCategory;
@@ -271,6 +286,126 @@ async function getMealByCategory() {
         select.appendChild(option);
       });
     });
+
+  select.addEventListener("change", (e) => {
+    selectedCategory = e.target.value;
+    const filteredData = `https://www.themealdb.com/api/json/v1/1/search.php?s=${selectedCategory}`;
+    if (
+      selectedCategory === "Dessert" ||
+      selectedCategory === "Miscellaneous" ||
+      selectedCategory === "Side" ||
+      selectedCategory === "Starter" ||
+      selectedCategory === "Pasta"
+    ) {
+      container.innerHTML = `<h1>Sorry, we don't have any ${selectedCategory} recipe</h1> 🥺`;
+    }
+
+    fetch(filteredData)
+      .then((response) => response.json())
+      .then((data) => {
+        const meals = data.meals;
+
+        meals.forEach((meal) => {
+          if (meal.strCategory === selectedCategory) {
+            container.innerHTML = "";
+            let i = 0;
+            for (let i = 0; i < meals.length; i++) {
+              rendrerData(meals[i]);
+            }
+            
+
+            container.childNodes.forEach((box) => {
+              box.addEventListener("click", (e) => {
+                container.style.display = "none";
+                secondContainer.style.display = "flex";
+                secondContainer.setAttribute("class", "secondContainer");
+
+                const data = e.target.data;
+
+                const mealImg = e.target.src;
+
+                const mealImgDiv = document.createElement("div");
+                mealImgDiv.setAttribute("class", "mealImgDiv");
+                const mealImgTag = document.createElement("img");
+                mealImgDiv.appendChild(mealImgTag);
+                mealImgTag.src = mealImg;
+                secondContainer.appendChild(mealImgDiv);
+
+                const mealTitleDiv = document.createElement("div");
+                mealTitleDiv.setAttribute("class", "mealTitleDiv");
+                const mealTitle = document.createElement("h1");
+                mealTitle.innerHTML = data.strMeal;
+                mealTitleDiv.appendChild(mealTitle);
+                secondContainer.appendChild(mealTitleDiv);
+                const insturctionDiv = document.createElement("div");
+                insturctionDiv.setAttribute("class", "insturctionDiv");
+                const insturction = document.createElement("p");
+                insturction.innerHTML = data.strInstructions;
+                insturctionDiv.appendChild(insturction);
+                secondContainer.appendChild(insturctionDiv);
+                const titleAndInsturctionDiv = document.createElement("div");
+                titleAndInsturctionDiv.setAttribute(
+                  "class",
+                  "titleAndInsturctionDiv"
+                );
+                titleAndInsturctionDiv.appendChild(mealTitleDiv);
+                titleAndInsturctionDiv.appendChild(insturctionDiv);
+                secondContainer.appendChild(titleAndInsturctionDiv);
+                const infoDiv = document.createElement("div");
+                infoDiv.setAttribute("class", "infoDiv");
+                const info = document.createElement("ul");
+                info.innerHTML = `
+                <li>Area: ${data.strArea}</li>
+                <li>Category: ${data.strCategory}</li>
+                <li>Tags: ${data.strTags}</li>
+                `;
+                if (data.strTags === null) {
+                  info.innerHTML = `
+                    <li>Area: ${data.strArea}</li>
+                    <li>Category: ${data.strCategory}</li>
+                    <li>Tags: No tags</li>
+                  `;
+                }
+
+                infoDiv.appendChild(info);
+                mealImgDiv.appendChild(infoDiv);
+                const ingredientsTitle = document.createElement("h2");
+                ingredientsTitle.innerHTML = "Ingredients:";
+                ingredientsTitle.setAttribute("class", "ingredientsTitle");
+                mealImgDiv.appendChild(ingredientsTitle);
+
+                const ingredientsDiv = document.createElement("div");
+                ingredientsDiv.setAttribute("class", "ingredientsDiv");
+                const ingredients = document.createElement("ul");
+                ingredientsDiv.appendChild(ingredients);
+                mealImgDiv.appendChild(ingredientsDiv);
+
+                for (let i = 1; i < 20; i++) {
+                  if (data[`strIngredient${i}`] !== "") {
+                    const ingredient = document.createElement("li");
+                    ingredient.innerHTML = `${data[`strIngredient${i}`]} - ${
+                      data[`strMeasure${i}`]
+                    }`;
+                    ingredients.appendChild(ingredient);
+                  }
+                }
+
+                const youtubeIfame = document.createElement("iframe");
+                youtubeIfame.setAttribute("class", "youtubeIfame");
+                youtubeIfame.src = `https://www.youtube.com/embed/${data.strYoutube.slice(
+                  -11
+                )}`;
+                if (data.strYoutube === null) {
+                  youtubeIfame.style.display = "none";
+                }
+
+                titleAndInsturctionDiv.appendChild(youtubeIfame);
+              });
+            });
+          }
+        });
+      });
+  });
 }
 
 getMealByCategory();
